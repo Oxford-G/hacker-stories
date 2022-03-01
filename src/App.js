e4cebbc00d2f153550f48bd14b57c2d6de367faa
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2022-03-01 22:08:37 +0100

    add list component

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import * as React from 'react'
+
+const welcome = {
+  greetings: 'Hey',
+  title: 'React'
+}
+
+const list = [
+  {  title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+]
+
+function App() {
+  return (
+    <div className="App">
+      <h1>{welcome.greetings} {welcome.title}</h1>
+
+      <label htmlFor="search">search:</label>
+    <input id="search" type="text"></input>
+
+    <hr />
+
+    < List />
+    
+    </div>
+  );
+}
+
+function List() {
+  return(
+    <ul>
+      {
+        list.map(function(item){
+          return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>  
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </li>
+          )
+        })
+      }
+    </ul>
+  )
+}
+export default App;
