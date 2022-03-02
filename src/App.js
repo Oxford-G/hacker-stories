d99023b0527b96bf3ec7014496721b012fd7b931
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2022-03-02 09:58:28 +0100

    refactored List component

--- src/App.js
+++ src/App.js
@@ -37,23 +37,23 @@ const App = () => (
   );
 
 const List = () => (
-    <ul>
-      {
-        list.map(function(item){
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>  
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          )
-        })
-      }
-    </ul>
-  )
+  <ul>
+    {
+      list.map(function(item){
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>  
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        )
+      })
+    }
+  </ul>
+  );
 
 function Search() {
   return (
