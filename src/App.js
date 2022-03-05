39ef3d042f1008c40e82ed77c98d396d0a607890
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2022-03-05 22:22:54 +0100

    add react side-effect

--- src/App.js
+++ src/App.js
@@ -26,11 +26,12 @@ const App = () => {
     }
   ];
 
-  const [searchTerm, setSearchTerm] = React.useState(localStorage.getItem('search') || 'React')
+  const [searchTerm, setSearchTerm] = React.useState(localStorage.getItem('search') || 'React');
+
+  React.useEffect(() => {localStorage.setItem('search', searchTerm)}, [searchTerm])
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
-    localStorage.setItem('search', event.target.value);
   }
 
   const searchedStories = stories.filter((story) => story.title.toLowerCase().includes(searchTerm.toLowerCase())
