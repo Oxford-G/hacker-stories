21c9114df6a1a436eb461d78b89ae597cb5787a3
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2022-03-09 18:51:31 +0100

    moved initialstories array to global field

--- src/App.js
+++ src/App.js
@@ -5,6 +5,25 @@ const welcome = {
   title: 'React'
 }
 
+const initialStories = [
+  {  title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+];
+
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(localStorage.getItem(key) || initialState);
 
