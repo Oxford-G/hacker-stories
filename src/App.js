5f42f3ca0cf1f01a0a1984b3ff2c7071193f985e
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2022-03-15 08:01:04 +0100

    used switch for usereducer function

--- src/App.js
+++ src/App.js
@@ -38,13 +38,7 @@ const useSemiPersistentState = (key, initialState) => {
 }
 
 const storiesReducer = (state, action) => {
-  if(action.type === 'SET_STORIES') {
-    return action.payload;
-  } else if(action.type === 'REMOVE_STORY'){
-    return state.filter(story => action.payload.objectID !== story.objectID)
-  } else{
-    throw new Error()
-  }
+  
 };
 
 const App = () => {
