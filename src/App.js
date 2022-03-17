5ebdbccecf2499acdbe591456ea70f5d248b0d4f
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2022-03-17 22:31:31 +0100

    add form element

--- src/App.js
+++ src/App.js
@@ -158,7 +158,7 @@ const App = () => {
   return (
   <div className="App">
     <h1>{welcome.greetings} {welcome.title}</h1>
-    <form onSubmit={hh}>
+    <form onSubmit={handleSearchSubmit}>
 
     </form>
     <InputWithLabel 
