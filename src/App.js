97722c7c83b8a96462ee181bf1e8d49b15e8a3f4
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2022-03-19 00:52:58 +0100

    add className to list

--- src/App.js
+++ src/App.js
@@ -244,12 +244,12 @@ const List = ({list, onRemoveItem}) => (
 
     return(
     <li className="item">
-    <span>
+    <span style={{width: '40%'}}>
       <a href={item.url}>{item.title}</a>  
     </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
+    <span style={{ width: '30%' }}>{item.author}</span>
+    <span style={{ width: '10%' }}>{item.num_comments}</span>
+    <span style={{ width: '10%' }}>{item.points}</span>
     <span>
       <button type="button" onClick={()=>onRemoveItem(item)}>Dismiss</button>
     </span>
