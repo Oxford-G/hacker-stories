cb0784531d843a5b188cb1b473cfd888e5cd4bf7
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2022-03-20 08:26:20 +0100

    add useRef hook to custom state

--- src/App.js
+++ src/App.js
@@ -108,6 +108,7 @@ font-size: 24px;
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 const useSemiPersistentState = (key, initialState) => {
+  
   const isMounted = React.useRef(false);
 
   const [value, setValue] = React.useState(localStorage.getItem(key) || initialState);
@@ -118,7 +119,6 @@ const useSemiPersistentState = (key, initialState) => {
     React.useEffect(() => {localStorage.setItem(key, value)}, [value, key])
   }
   
-
   return [value, setValue]
 }
 
