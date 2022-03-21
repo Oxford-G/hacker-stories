27a446578db04af9c68128507490e47016ef17bb
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2022-03-21 11:34:09 +0100

    add itemProp type

--- src/App.tsx
+++ src/App.tsx
@@ -348,22 +348,30 @@ const List = React.memo(
   )
 );
 
-  const Item = ({item, onRemoveItem}) => {
+type ItemProps = {
+  item: Story;
+  onRemoveItem: (item: Story) => void;
+};
 
-    return(
-    <StyledItem>
-    <StyledColumn width="40%">
-      <a href={item.url}>{item.title}</a>  
-    </StyledColumn>
-    <StyledColumn width="30%">{item.author}</StyledColumn>
-    <StyledColumn width="10%">{item.num_comments}</StyledColumn>
-    <StyledColumn width="10%">{item.points}</StyledColumn>
-    <StyledColumn width="10%">
-      <StyledButtonSmall type="button" onClick={()=>onRemoveItem(item)}>
-      <Check height="18px" width="18px"/>
-      </StyledButtonSmall>
-    </StyledColumn>
-    </StyledItem>
-  )};
+const Item = (
+  {item,
+  onRemoveItem
+  }: ItemProps) => {
+
+  return(
+  <StyledItem>
+  <StyledColumn width="40%">
+    <a href={item.url}>{item.title}</a>  
+  </StyledColumn>
+  <StyledColumn width="30%">{item.author}</StyledColumn>
+  <StyledColumn width="10%">{item.num_comments}</StyledColumn>
+  <StyledColumn width="10%">{item.points}</StyledColumn>
+  <StyledColumn width="10%">
+    <StyledButtonSmall type="button" onClick={()=>onRemoveItem(item)}>
+    <Check height="18px" width="18px"/>
+    </StyledButtonSmall>
+  </StyledColumn>
+  </StyledItem>
+)};
 
 export default App;
