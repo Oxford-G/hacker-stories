518433fcf0076e814285c03083d47de28d46c062
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2022-03-24 23:43:14 +0100

    add callback test

--- src/App.test.js
+++ src/App.test.js
@@ -91,5 +91,13 @@ describe('storiesReducer', () => {
 describe('Item', ()=> {
   test('renders all properties', ()=> {
     render(<Item item={storyOne} />)
+
+    expect(screen.getByText('Jordan Walke')).toBeInTheDocument()
+    expect(screen.getByText('React')).toHaveAttribute('href', 'https://reactjs.org/')
   });
+
+  test('renders a clickable dismiss button', ()=> {
+    render(< Item item={stories}/>)
+
+  })
 });
