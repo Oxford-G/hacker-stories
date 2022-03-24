26fc13ae9863ae0f9b4d2f60758a7dd2d11cd33c
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2022-03-24 17:23:00 +0100

    add remove_story test case

--- src/App.test.js
+++ src/App.test.js
@@ -16,3 +16,41 @@ describe('something truthy and falsy', () => {
     expect(false).toBe(false)
   })
 })
+
+const storyOne = {
+  title: 'React',
+  url: 'https://reactjs.org/',
+  author: 'Jordan Walke',
+  num_comments: 3,
+  points: 4,
+  objectID: 0,
+};
+
+const storyTwo = {
+  title: 'Redux',
+  url: 'https://redux.js.org/',
+  author: 'Dan Abramov, Andrew Clark',
+  num_comments: 2,
+  points: 5,
+  objectID: 1,
+};
+
+const stories = [storyOne, storyTwo];
+
+describe('storiesReducer', () => {
+  test('removes a story from all stories', () => {
+    const action = { type: 'REMOVE_STORY', payload: storyOne };
+
+    const state = { data: stories, isLoading: false, isError: false };
+
+    const newState = storiesReducer(state, action);
+
+    const expectedState = {
+    data: [storyTwo],
+    isLoading: false,
+    isError: false,
+    };
+
+    expect(newState).toStrictEqual(expectedState);
+  });
+});
